7c05120e65cb47ae9574bb82437424fbacb5593c
SofiiaPotapova <canaryfinch@gmail.com>
2020-05-09 13:54:27 +0300

    Added strip terms

--- single_threaded.py
+++ single_threaded.py
@@ -1,4 +1,5 @@
 from glob import glob
+from pprint import pprint as pp
 
 
 def single_pass_indexing(fileglob='**\*.txt'):
@@ -9,13 +10,15 @@ def single_pass_indexing(fileglob='**\*.txt'):
             doc_id = txtfile
             text = f.read().split()
             for term in text:
-                if term in terms:
-                    index[term].append(doc_id)
+                done_term = term.strip("(){}[],.!?<>:;|/\\")
+                if done_term in terms:
+                    index[done_term].append(doc_id)
                 else:
-                    terms.append(term)
-                    index[term] = []
-                    index[term].append(doc_id)
+                    terms.append(done_term)
+                    index[done_term] = []
+                    index[done_term].append(doc_id)
     return index
 
 
 final = single_pass_indexing()
+pp(final)
